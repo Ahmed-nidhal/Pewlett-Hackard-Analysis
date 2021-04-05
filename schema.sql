9cd78419c25e11d2bb233ed491096d41d378c920
Ahmed-nidhal <78449998+Ahmed-nidhal@users.noreply.github.com>
2021-04-05 14:11:33 -0400

    retirement_info

--- schema.sql
+++ schema.sql
@@ -38,38 +38,80 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
-
 CREATE TABLE dept_emp (
   emp_no INT NOT NULL,
-  dept_no INT NOT NULL,
+  dept_no varchar NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (dept_no,emp_no)
+	);
+
+CREATE TABLE titles (
+  emp_no varchar NOT NULL,
+  title int NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (emp_no) REFERENCES salaries (emp_no)
+PRIMARY KEY (emp_no)
 );
+
 
 
 CREATE TABLE titles (
-  emp_no INT NOT NULL,
+  emp_no int NOT NULL,
   title varchar NOT NULL,
   from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  to_date DATE NOT NULL
 );
-
-
-CREATE TABLE titles (
-  emp_no INT NOT NULL,
-  title varchar NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
-);
-
 
 
 SELECT * FROM departments;
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1955-12-31';
+
+
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+and (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+SELECT count (first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+and (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+select * from retirement_info;
+
 
 
